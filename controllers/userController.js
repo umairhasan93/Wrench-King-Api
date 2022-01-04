c8ab0d467c9716d0108ad7189113d3a394fd2c52
Muhammad Umair Hasan <umairhasan463@gmail.com>
2022-01-04 21:27:39 +0500

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,10 +36,12 @@ const authUser = asyncHandler(async (req, res) => {
 
     if (user && (await user.matchPassword(password))) {
         res.json({
+            id: user._id,
             fname: user.firstname,
             lname: user.lastname,
             name: user.firstname + ' ' + user.lastname,
             contact: user.contact,
+            password: user.password,
             username: user.username,
             email: user.email,
             role: user.role,
@@ -51,5 +53,22 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+const findUser = asyncHandler(async (req, res) => {
+    User.findOne({ _id: req.params._id }, function (err, user) {
+        if (err) return res.status(500).send("There was a problem finding the Mechanic.");
+        res.status(200).send(user);
+    })
+})
 
-module.exports = { registerUser, authUser }
+const updateUser = asyncHandler(async (req, res) => {
+    console.log(req.params._id);
+    console.log(req.body);
+    const user = await User.findByIdAndUpdate(req.params._id, req.body, { new: true });
+    if (!user) {
+        return res.status(500).send("There's a problem Updating User")
+    }
+    res.status(200).send(user);
+})
+
+
+module.exports = { registerUser, authUser, findUser, updateUser }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { registerUser, authUser } = require('../controllers/userController');
+const { registerUser, authUser, findUser, updateUser } = require('../controllers/userController');
 
 const router = express.Router()
 
 router.route('/register').post(registerUser)
 router.route('/login').post(authUser)
+router.route('/:_id').put(updateUser)
+router.route('/:_id').get(findUser)
 
 module.exports = router;

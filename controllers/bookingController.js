614135ff21afd7f7411e6c57a2081a7b86a6a531
Muhammad Umair Hasan <umairhasan463@gmail.com>
2022-05-18 18:45:51 +0500

    make it better

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -40,10 +40,11 @@ const findPendingBooking = asyncHandler(async (req, res) => {
 
 const findMechanicPendingBooking = asyncHandler(async (req, res) => {
     console.log(req.params._id);
-    Booking.find({ "Mechanic_Number": req.params._id, Status: 'Pending' }, function (err, booking) {
+    const d = new Date()
+    const date = d.getDate() + '-' + '0' + (d.getMonth() + 1) + '-' + d.getFullYear()
+    Booking.find({ "Mechanic_Number": req.params._id, Status: 'Pending', "Requested_Date": date }, function (err, booking) {
         if (err) return res.status(500).send("Failed Fetching Data")
         res.status(200).send(JSON.stringify(booking))
-
     })
 })
 
